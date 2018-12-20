e83ad276a2cb33f0cbf67b1bbada5e6433b21e78
Haorui <353630611@qq.com>
2018-12-20 23:36:39 +0500

    Start

--- app/src/main/java/com/example/haojialiang/memomyapplication/AddMemo.java
+++ app/src/main/java/com/example/haojialiang/memomyapplication/AddMemo.java
@@ -1,70 +1,239 @@
 package com.example.haojialiang.memomyapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
 
 public class AddMemo extends AppCompatActivity {
     @BindView(R.id.et_adt_title)
     EditText etAdtTitle;
     @BindView(R.id.et_adt_content)
     EditText etAdtContent;
-    @BindView(R.id.tv_adt_date)
-    TextView tvAdtDate;
+//    @BindView(R.id.tv_adt_date)
+//    TextView tvAdtDate;
     @BindView(R.id.btn_adt_save)
     Button btnAdtSave;
     int mYear = 2018;
     int mMonth = 12;
     int mDay = 1;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        ButterKnife.bind(this);
-        super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_add_memo);
-    }
+    private TextView tvDate ;
+
+    public static final int PHOTO_REQUEST_CAREMA = 1;// 拍照
+    public static final int CROP_PHOTO = 2;
+    private Button takePhoto;
+    private ImageView picture;
+    private Uri imageUri;
+
+    private static int REQUEST_PERMISSION_CODE = 1;
+
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.CAMERA,
+
+    };
+
+    public static File tempFile;
 
 
-    @OnClick({R.id.tv_adt_date, R.id.btn_adt_save})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.tv_adt_date:
-                new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        tvAdtDate.setText(year + "-" + month + "-" + dayOfMonth);
-
-                    }
-                }, mYear, mMonth-1, mDay).show();
-                break;
-            case R.id.btn_adt_save:
-//                showLoading();
-                switch (btnAdtSave.getText().toString()) {
-//                    case "Add":
-//                        mPresenter.addTask(etAdtTitle.getText().toString(), etAdtContent.getText().toString(), tvAdtDate.getText().toString(), mCustomType);
-//                        break;
-//                    case "Update":
-//                        mPresenter.updateTask(todoSection.t.getId(), etAdtTitle.getText().toString(), etAdtContent.getText().toString(), tvAdtDate.getText().toString(), todoSection.t.getStatus(), mEditType);
-//                        break;
-                }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_show:
+                openCamera(this);
                 break;
         }
     }
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    public void addpicture(View view) {
+        setContentView(R.layout.activity_add_memo);
+        init();
+
     }
 
+    private void init() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
+            }
+        }
+
+
+        tvDate = findViewById(R.id.tv_adt_date);
+        picture=findViewById(R.id.iv_show);
+
+    }
+
+
+    public static boolean hasSdcard() {
+        return Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED);
+    }
+
+
+
+    public void openCamera(Activity activity) {
+        //獲取系統版本
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        // 激活相机
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // 判断存储卡是否可以用，可用进行存储
+        if (hasSdcard()) {
+            SimpleDateFormat timeStampFormat = new SimpleDateFormat(
+                    "yyyy_MM_dd_HH_mm_ss");
+            String filename = timeStampFormat.format(new Date());
+            tempFile = new File(Environment.getExternalStorageDirectory(),
+                    filename + ".jpg");
+            if (currentapiVersion < 24) {
+                // 从文件中创建uri
+                imageUri = Uri.fromFile(tempFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            } else {
+                //兼容android7.0 使用共享文件的形式
+                ContentValues contentValues = new ContentValues(1);
+                contentValues.put(MediaStore.Images.Media.DATA, tempFile.getAbsolutePath());
+                //检查是否有存储权限，以免崩溃
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    //申请WRITE_EXTERNAL_STORAGE权限
+                    Toast.makeText(this,"请开启存储权限",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                imageUri = activity.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+//                picture.setImageBitmap(bitmap);
+            }
+        }
+        // 开启一个带有返回值的Activity，请求码为PHOTO_REQUEST_CAREMA
+        activity.startActivityForResult(intent, PHOTO_REQUEST_CAREMA);
+    }
+
+
+//    @OnClick({R.id.tv_adt_date, R.id.btn_adt_save})
+//    public void onViewClicked(View view) {
+//        switch (view.getId()) {
+//            case R.id.tv_adt_date:
+//                new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                        tvAdtDate.setText(year + "-" + month + "-" + dayOfMonth);
+//
+//                    }
+//                }, mYear, mMonth - 1, mDay).show();
+//                break;
+//            case R.id.btn_adt_save:
+////                showLoading();
+//                switch (btnAdtSave.getText().toString()) {
+////                    case "Add":
+////                        mPresenter.addTask(etAdtTitle.getText().toString(), etAdtContent.getText().toString(), tvAdtDate.getText().toString(), mCustomType);
+////                        break;
+////                    case "Update":
+////                        mPresenter.updateTask(todoSection.t.getId(), etAdtTitle.getText().toString(), etAdtContent.getText().toString(), tvAdtDate.getText().toString(), todoSection.t.getStatus(), mEditType);
+////                        break;
+//                }
+//                break;
+//        }
+
+
+
+    public void addpicture(View view) {
+
+
+        openCamera(this);
+
+
+        }
+
     public void addmemo(View view) {
+
+
+    }
+
+    public void pickdate(View view) {
+        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                tvDate.setText(year + "-" + month + "-" + dayOfMonth);
+
+            }
+        }, mYear, mMonth - 1, mDay).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            for (int i = 0; i < permissions.length; i++) {
+                Log.i("MainActivity", "申请的权限为：" + permissions[i] + ",申请结果：" + grantResults[i]);
+            }
+        }
+
+}
+}
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri, "image/*");
+                    intent.putExtra("scale", true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CROP_PHOTO); // 启动裁剪程序
+                }
+                break;
+            case CROP_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                                .openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/haojialiang/memomyapplication/MainActivity.java
+++ app/src/main/java/com/example/haojialiang/memomyapplication/MainActivity.java
@@ -19,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         ButterKnife.bind(this);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
