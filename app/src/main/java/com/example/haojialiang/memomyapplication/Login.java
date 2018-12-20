88b0cb35b593df219ef0acfc186e09abc1e73525
Navruz Jn <navruzjn@Navruz-Jn.local>
2018-12-20 23:33:41 +0500

    MemoDb

--- app/src/main/java/com/example/haojialiang/memomyapplication/Login.java
+++ app/src/main/java/com/example/haojialiang/memomyapplication/Login.java
@@ -2,12 +2,36 @@ package com.example.haojialiang.memomyapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class Login extends AppCompatActivity {
+
+    @BindView(R.id.tv_al_account)
+    AutoCompleteTextView tvAlAccount;
+    @BindView(R.id.tv_al_password)
+    EditText tvAlPassword;
+    @BindView(R.id.login)
+    Button login;
+    @BindView(R.id.regitster)
+    TextView regitster;
+    @BindView(R.id.email_login_form)
+    LinearLayout emailLoginForm;
+    @BindView(R.id.login_form)
+    ScrollView loginForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
--- app/src/main/java/com/example/haojialiang/memomyapplication/MainActivity.java
+++ app/src/main/java/com/example/haojialiang/memomyapplication/MainActivity.java
@@ -3,12 +3,11 @@ package com.example.haojialiang.memomyapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import butterknife.ButterKnife;
 
@@ -27,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent intent = new Intent(MainActivity.this, AddMemo.class);
                 startActivity(intent);
             }
@@ -36,19 +34,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
